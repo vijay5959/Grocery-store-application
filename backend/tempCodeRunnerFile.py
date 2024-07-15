21672518d8a3cd640891f8fa044f15d1c6fd6c6e
vijay5959 <vijaybawasker85@gmail.com>
2024-07-15 10:48:44 +0530

    complete project

---
+++ backend/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+
+@app.route('/getUOM', methods=['GET'])
+def get_uom():
+    response = uom_dao.get_uoms(connection)
+    response = jsonify(response)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
